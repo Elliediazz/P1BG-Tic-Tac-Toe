9daa11d733141d127d62e2478f7b0445cd1f552f
Ellie Diaz <elliediaz@Ellies-Air.lan>
2022-04-19 20:57:10 -0500

    Uploading first files

---
+++ TTT.js
@@ -0,0 +1,23 @@
+//Place for Player 1 and Player 2 to enter their names
+
+//randommize which player begins 
+
+//Player 1 chooses "X" or "O"
+
+//If player 1 choosed "X" then player 2 is assigned "O" 
+//else player if player 1 chooses "O" then player 2 is assigned "X"
+
+// Diplay the players assigned mark next to their name
+
+//the player who chose last, makes the first move
+
+// if player 1 clicks a certain area of the board, their designated mark will appear
+
+//turns switch
+
+//if three "o"s or three "x"s connect, display "Player X Wins!"
+
+//if no three marks connect display "Draw"
+
+// add a restart button
+
