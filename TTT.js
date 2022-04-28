a41c07511f8b097691085a063625336edbab59bf
Ellie Diaz <elliediaz@Ellies-MacBook-Air.local>
2022-04-28 13:47:08 -0500

    added functionality (x/o on click) to board/restart button

--- TTT.js
+++ TTT.js
@@ -1,52 +1,33 @@
-//place for Player 1 and Player 2 to enter their names
-    document.getElementById ("playerOne").addEventListener("click", function(){
-        let playerOne = window.prompt("PLAYER ONE")
-        document.getElementById("outputPlayerOne").innerHTML = playerOne
-    })
-
-    document.getElementById ("playerTwo").addEventListener("click", function(){
-        let playerTwo = window.prompt("PLAYER ONE")
-        document.getElementById("outputPlayerTwo").innerHTML = playerTwo
-    })
-
-//randommize which player begins 
-
-//Player 1 chooses "X" or "O"
-
-//if player 1 choosed "X" then player 2 is assigned "O" 
-//else player if player 1 chooses "O" then player 2 is assigned "X"
-
-// diplay the players assigned mark next to their name
-
-//the player who chose last, makes the first move
-
 // if player 1 clicks a certain area of the board, their designated mark will appear
-let myBoardArray = [
-    document.getElementById("board1"),
-    document.getElementById("board2"),
-    document.getElementById("board3"),
-    document.getElementById("board4"),
-    document.getElementById("board5"),
-    document.getElementById("board6"),
-    document.getElementById("board7"),
-    document.getElementById("board8"),
-    document.getElementById("board9"),
-]
+//defining variables
+let turn = 'X';
+let winner = null;
+ 
+//created an array for me for which i can use to loop over when clicked
+let boards = document.querySelectorAll('.board').forEach(board => {
+    board.addEventListener('click', function(){
+        //write a function to make  'X'/'O' appear and switch turns
+        //console.log('hi')
+        if (turn === 'X'){
+            board.innerHTML='X';
+            turn = 'O'
+        } else {
+            board.innerHTML='O';
+            turn = 'X'
+        }
+    })
 
-function markBoard(){
-    alert("hello")
-}
+    // give functionality to restart button
+    let reset = document.getElementById('restartBtn');
+        reset.addEventListener('click', function(){
+        //Write a function to reset the board
+        //console.log('hello world');
+        board.innerHTML= "";
+        turn = 'X'
+        winner = null;
+    })
 
-for (let i=0; i < myBoardArray.length; i++){
-    myBoardArray.addEventListener("click", function(){markBoard();})
-}
-
-
-//turns switch
-
-//if three "o"s or three "x"s connect, display "Player X Wins!"
+}) 
+//if three "o"s or three "x"s connect, display "Player X/O Wins!"
 
 //if no three marks connect display "Draw"
-
-// add a restart button
-
